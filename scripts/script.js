7d5c09ea32ef876de69acd145ef00af2de29eabd
Shomu1999 <soumyadeepdas.259@gmail.com>
2026-06-27 14:14:18 +0530

    Prelim update

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let home = document.getElementById("home");
+home.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+let about = document.getElementById("about");
+about.addEventListener("click", function() {
+    window.location.href = "about.html";
+});
